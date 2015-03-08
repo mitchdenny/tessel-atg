01dc21e18819e122a64bf35d1decdff9c5f05f30
Mitch Denny <mitch@mitchdenny.com>
2015-03-08 22:07:33 +1100

    Building out the Bluetooth functionality.

--- app.js
+++ app.js
@@ -4,17 +4,17 @@ var publishers = require('./lib/publishers');
 var sen10737p = require('tessel-sen10737p');
 var atg = require('./lib/monitor');
 
-// // Get the Bluetooth publisher configured.
-// var blePort = tessel.port['B'];
-// var controller = ble.use(blePort);
-// var publisher = publishers.createBluetoothPublisher(controller);
+// Get the Bluetooth publisher configured.
+var blePort = tessel.port['B'];
+var controller = ble.use(blePort);
+var publisher = publishers.createBluetoothPublisher(controller);
 
-// Get the Event Hub publisher configured.
-var namespace = 'tessel-atg';
-var eventHub = 'tessel-atg-tenant1';
-var device = 'tessel-atg-tenant1-device1';
-var sharedAccessSignature = 'SharedAccessSignature sr=https%3A%2F%2Ftessel-atg.servicebus.windows.net%2Ftessel-atg-tenant1%2Fpublishers%2Ftessel-atg-tenant1-device1%2Fmessages&sig=EPKqXdzcCzzgCJRAome1kdbGxbqAtcZyaT2oKP3f7UY%3D&se=1454612852&skn=tessel-atg-tenant1-policy1';
-var publisher = publishers.createEventHubPublisher(namespace, eventHub, device, sharedAccessSignature);
+// // Get the Event Hub publisher configured.
+// var namespace = 'tessel-atg';
+// var eventHub = 'tessel-atg-tenant1';
+// var device = 'tessel-atg-tenant1-device1';
+// var sharedAccessSignature = 'SharedAccessSignature sr=https%3A%2F%2Ftessel-atg.servicebus.windows.net%2Ftessel-atg-tenant1%2Fpublishers%2Ftessel-atg-tenant1-device1%2Fmessages&sig=EPKqXdzcCzzgCJRAome1kdbGxbqAtcZyaT2oKP3f7UY%3D&se=1454612852&skn=tessel-atg-tenant1-policy1';
+// var publisher = publishers.createEventHubPublisher(namespace, eventHub, device, sharedAccessSignature);
 
 // Get the ultrasonic ranger up and running.
 var gpioPort = tessel.port['GPIO'];
